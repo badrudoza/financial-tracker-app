56c7720a6b8e731e59ad4667c61b9157a567a264
bda <badrudozaansari0786@gmail.com>
2026-04-25 16:12:19 +0530

    initial commit

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -14,6 +14,7 @@ import AddExpenseModal from "../Modals/AddExpense";
 import AddIncomeModal from "../Modals/addIncome";
 
 
+
 export default function Dashboard() {
   const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
   const [isIncomeModalVisible, setIsIncomeModalVsible] = useState(false);
