28243bf2dea3e0c67afbf25b90ca5623dec66c3b
bda <badrudozaansari0786@gmail.com>
2026-05-02 16:29:12 +0530

    fix images

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.css";
 import { auth } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { signOut } from "firebase/auth";
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import img from "@/assets/DefaultImages.jpg";
 
 export default function Header() {
   const [user, loading] = useAuthState(auth);
   const navigate = useNavigate();
+
   useEffect(() => {
     if (user) {
       navigate("/dashboard");
@@ -18,26 +17,25 @@ export default function Header() {
   }, [user, loading]);
 
   function logoutFunc() {
-    try {
-      signOut(auth)
-        .then(() => {
-          toast.success("User Logout succesfully");
-          navigate("/");
-        })
-        .catch((error) => {
-          // An error happened.
-          toast.error(error);
-        });
-    } catch (e) {
-      toast.error(e.message);
-    }
+    signOut(auth)
+      .then(() => {
+        toast.success("User Logout successfully");
+        navigate("/");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
   }
+
   return (
     <div className="navbar">
       <p className="logo">Financely.</p>
       {user && (
-        <div style={{display:'flex',alignItems:"center",gap:"0.75rem"}}>
-          <img src={user.photoURL?user.photoURL:img} style={{borderRadius:"50%",height:"1.5rem",width:"1.5rem"}} />
+        <div style={{ display: "flex", alignItems: "center", gap: "0.75rem" }}>
+          <img
+            src={user.photoURL ? user.photoURL : "/DefaultImages.jpg"}
+            style={{ borderRadius: "50%", height: "1.5rem", width: "1.5rem" }}
+          />
           <p className="logo link" onClick={logoutFunc}>
             Logout
           </p>
